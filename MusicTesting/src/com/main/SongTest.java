13502a766f32b5ea690ffe02319fc011d9623f24
nathan albin <nathan@radhet.com>
2020-07-27 14:21:22 -0400

    created spring project. created java project to test midi file generation methods to be used in application

---
+++ MusicTesting/src/com/main/SongTest.java
@@ -0,0 +1,12 @@
+package com.main;
+
+import com.songtest.better.BetterSongMethod;
+
+public class SongTest {
+
+	public static void main(String[] args) {
+		BetterSongMethod.generateSong(8);
+		BetterSongMethod.generateChords(3);
+	}
+
+}
